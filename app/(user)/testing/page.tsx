a6a26f036e5c3f919dda2c12ca4892bcda849065
sk <mrrkhann9u@email.com>
2024-06-17 13:57:19 +0700

    add more review feature

--- app/(user)/testing/page.tsx
+++ app/(user)/testing/page.tsx
@@ -1,10 +1,12 @@
 import CardDetailComponent from '@/components/card/CardDetailComponent'
+import ReviewProductDetailComponent from '@/components/card/review/ReviewProductDetailComponent'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
       <CardDetailComponent/>
+      <ReviewProductDetailComponent/>
     </div>
   )
 }
---
+++ components/card/review/review.ts
@@ -0,0 +1,64 @@
+
+  
+  export const Reviews = [
+    {
+      name: "Angela",
+      date: "June 17, 2024",
+      imageUrl: "https://i.pinimg.com/564x/0a/ba/ca/0abacac387db067c3d4892f832633bc9.jpg",
+      comment: "I Really Love This Product!",
+      star: 4.5,
+      description: "The product arrived quickly and was exactly as described. I'm very satisfied with my purchase and will be a repeat customer.",      
+      commentImages: [
+        "https://i.pinimg.com/564x/f8/ed/62/f8ed62cc8f6ab855e27dd211b4b411e1.jpg",
+        "https://i.pinimg.com/736x/e9/27/25/e92725c60c7a16bd6604381aee49f977.jpg"
+      ]
+    },
+    {
+      name: "Puttrea",
+      date: "July 5, 2024",
+      imageUrl: "https://i.pinimg.com/564x/07/b3/7b/07b37bf781f4c72ba2655337e5e9cb21.jpg",
+      comment: "Nice product. I recommend it.",
+      star: 5.0,
+      description: "All hand-made with natural soy wax, Candleaf is made for your pleasure moments All hand-made with natural soy wax, Candleaf is made for your pleasure moments with natural soy wax, Candleaf is made for your pleasure moment ",
+      commentImages: [
+        "https://i.pinimg.com/564x/9e/3f/56/9e3f560d5c816976a4de9d339f688667.jpg"
+      ]
+    }
+    ,
+  {
+    name: "Sarah Lee",
+    date: "August 12, 2024",
+    imageUrl: "https://i.pinimg.com/564x/23/8e/7a/238e7a616440b790adf0cdff3fa4c32a.jpg",
+    comment: "Excellent quality and great customer service!",
+    star: 4.8,
+    description: "I'm really impressed with the quality of the product. It exceeded my expectations. The customer service was also very responsive and helpful.",
+    commentImages: [
+      "https://i.pinimg.com/564x/7e/55/a2/7e55a297a066a40b60cb038493ad709e.jpg"
+    ]
+  },
+  {
+    name: "Michael Johnson",
+    date: "September 25, 2024",
+    imageUrl: "https://i.pinimg.com/564x/40/c9/50/40c950daf56cb009ce8ddb3341892f4b.jpg",
+    comment: "Impressive product! Will definitely buy again.",
+    star: 4.3,
+    description: "The product arrived quickly and was exactly as described. I'm very satisfied with my purchase and will be a repeat customer.",
+    commentImages: [
+      "https://i.pinimg.com/564x/4d/9f/55/4d9f55b0c46e5569d076b34c73314367.jpg",
+      "https://i.pinimg.com/564x/79/9d/52/799d5212602ed1414fa22efd49fdea55.jpg"
+
+    ]
+  },
+  {
+    name: "Emily Wilson",
+    date: "October 10, 2024",
+    imageUrl: "https://i.pinimg.com/564x/66/f5/21/66f52119c4857faf10f7c53fff89f1b0.jpg",
+    comment: "Beautiful product! Highly recommend it.",
+    star: 4.7,
+    description: "The product is not only beautiful but also functional. It fits perfectly with my decor and is of excellent quality.",
+    commentImages: [
+      "https://i.pinimg.com/564x/ba/f3/b5/baf3b5b520440ff559c4a7eaf1325962.jpg"
+    ]
+  }
+  ];
+  

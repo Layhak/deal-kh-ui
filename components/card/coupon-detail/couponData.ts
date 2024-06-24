8f64457d2c0c6c2a44fb876075c8dbbb16474fb3
sk <mrrkhann9u@email.com>
2024-06-25 01:34:34 +0700

    card coupons and card coupon detail

---
+++ components/card/coupon-detail/couponData.ts
@@ -0,0 +1,41 @@
+// couponDetail.ts
+
+export type CouponData = {
+  id: number;
+  image: string; // Single image URL
+};
+
+export const couponData: CouponData[] = [
+  {
+    id: 1,
+    image: "https://as1.ftcdn.net/v2/jpg/02/91/67/60/1000_F_291676069_ZBjqhpjsPBprZlfjmbOTt4zebYG5gGq5.jpg"
+  },
+  {
+    id: 2,
+    image: "https://as1.ftcdn.net/v2/jpg/02/91/67/62/1000_F_291676254_0OPBGYugiXq8GVLW69Etyj98yXlfvZxm.jpg"
+  },
+  {
+    id: 3,
+    image: "https://as2.ftcdn.net/v2/jpg/02/91/67/83/1000_F_291678347_ldV3Fs2UXsVMe1fpkoTcluIjNh65z6ya.jpg"
+  },
+  {
+    id: 4,
+    image: "https://as1.ftcdn.net/v2/jpg/03/05/13/86/1000_F_305138615_3S2eG484NTtMRupLZB4odKxboyBqhVbb.jpg"
+  },
+  {
+    id: 5,
+    image: "https://as2.ftcdn.net/v2/jpg/02/91/67/61/1000_F_291676143_GtsMEfoFeRQADmmeLg7MSiOxYE1ET9hZ.jpg"
+  },
+  {
+    id: 6,
+    image: "https://as2.ftcdn.net/v2/jpg/02/91/67/65/1000_F_291676535_WzMlxsrQh8Ww7V04HvR5owHArtWcZkuv.jpg"
+  },
+  {
+    id: 7,
+    image: "https://as2.ftcdn.net/v2/jpg/02/91/67/59/1000_F_291675907_a42y1cNQP2VRH2t5nNoVZsmkshVrHwVh.jpg"
+  },
+  {
+    id: 8,
+    image: "https://as1.ftcdn.net/v2/jpg/02/91/67/60/1000_F_291676097_ycnF7xaUPfzSMWp6X8v94EOvWgtI9Szx.jpg"
+  }
+];

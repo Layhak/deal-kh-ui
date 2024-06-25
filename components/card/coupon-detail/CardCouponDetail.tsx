aac4ce1269eefce702232386a5030f2889192d61
sk <mrrkhann9u@email.com>
2024-06-25 22:23:27 +0700

    has done coupons list, coupons detail, and responsive

--- components/card/coupon-detail/CardCouponDetail.tsx
+++ components/card/coupon-detail/CardCouponDetail.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 type CouponData = {
   id: number;
-  images: string[];
+  images: string;
   name: string;
   duration: string;
   location: string;
@@ -17,29 +17,37 @@ type Props = {
 
 const CardCouponDetail: React.FC<Props> = ({ coupon }) => {
   return (
-    <div className="mt-6 overflow-hidden rounded border-2 bg-white p-8">
-      <div className="">
-        <div className="coupon-container flex flex-col justify-between lg:flex-row lg:pb-10">
-          {coupon.images.map((imageUrl, index) => (
-            <div
-              key={index}
-              className="mb-4 overflow-hidden rounded-md border border-gray-200 lg:mb-0 lg:mr-4"
-            >
-              <Image
-                className="h-[250px] w-[550px] object-cover"
-                src={imageUrl}
-                alt={`${coupon.name} Image ${index + 1}`}
-              />
+    <div className="flex items-center justify-center h-screen">
+      <div className="overflow-hidden rounded bg-white p-8">
+        <div className="grid grid-cols-1 content-center gap-8 lg:grid-cols-2">
+          {/* Product Image */}
+          <div className="rounded-lg">
+            <Image
+              src={coupon.images}
+              alt={coupon.name}
+              className="h-full w-full rounded-lg"
+            />
+          </div>
+
+          {/* Product Details */}
+          <div className="my-auto">
+            {/* Product information */}
+            <div className=''>
+              <h1 className="text-2xl font-semibold text-gray-700 dark:text-white md:text-3xl">
+                {coupon.name}
+              </h1>
+              {/* Shop and other details */}
+              <div className="mt-4">
+                <ul className="px-4 list-disc text-gray-600 dark:text-gray-300">
+                  <li>{coupon.duration}</li>
+                  <li>{coupon.location}</li>
+                  <li>{coupon.contact}</li>
+                  <li>{coupon.type}</li>
+                </ul>
+              </div>
             </div>
-          ))}
+          </div>
         </div>
-      </div>
-      <div className="px-6 py-4">
-        <div className="mb-2 text-xl font-bold">{coupon.name}</div>
-        <p className="mb-2 text-base text-gray-700">{coupon.duration}</p>
-        <p className="mb-2 text-base text-gray-700">{coupon.location}</p>
-        <p className="mb-2 text-base text-gray-700">{coupon.contact}</p>
-        <p className="mb-2 text-base text-gray-700">{coupon.type}</p>
       </div>
     </div>
   );

76532647f387545eb43bd3afc768aa7e53527d37
Heng Layhak <layhakheng@gmail.com>
2024-07-30 16:53:57 +0700

    update image size to make the image show in facebook (1)

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,7 @@
 export const siteConfig = {
   name: 'Deal Kh',
-  description: 'Find the best deals, coupons, and promotions from shops in Cambodia. Save big on your shopping with exclusive offers and discounts at DealKH.',
+  description:
+    'Find the best deals, coupons, and promotions from shops in Cambodia. Save big on your shopping with exclusive offers and discounts at DealKH.',
   navItems: [
     { label: 'Home', href: '/', tooltip: 'Go to Home Page' },
     { label: 'Deal', href: '/deal', tooltip: 'Go to Discount Page' },
@@ -20,16 +21,23 @@ export const siteConfig = {
   ],
   metadata: {
     title: 'Dealkh Istad',
-    description: 'Find the best deals, coupons, and promotions from shops in Cambodia. Save big on your shopping with exclusive offers and discounts at DealKH.',
+    description:
+      'Find the best deals, coupons, and promotions from shops in Cambodia. Save big on your shopping with exclusive offers and discounts at DealKH.',
     keywords: ['Dealkh', 'Deal kh', 'Deal-kh', 'dealkh istad', 'dealkh-istad'],
     openGraph: {
       type: 'website',
       locale: 'en_US',
       url: 'https://www.dealkh.istad.co/',
       title: 'Istad DealKH | Platform for Cambodian Deals',
-      description: 'Discover the latest deals, coupons, and promotions from shops in Cambodia at DealKH. Save on your shopping with our curated list of discounts and offers.',
+      description:
+        'Discover the latest deals, coupons, and promotions from shops in Cambodia at DealKH. Save on your shopping with our curated list of discounts and offers.',
       images: [
-        { url: process.env.NEXT_PUBLIC_LOGO_URL + 'banner.png', alt: 'DealKH Logo Ecommerce Website' },
+        {
+          url: process.env.NEXT_PUBLIC_LOGO_URL + 'banner.png',
+          width: 1200,
+          height: 630,
+          alt: 'DealKH Logo Ecommerce Website',
+        },
         // { url: process.env.NEXT_PUBLIC_LOGO_URL + 'logo.png', alt: 'Exclusive Shopping Deals in Cambodia' },
       ],
       siteName: 'dealkh.istad.co',
@@ -37,9 +45,13 @@ export const siteConfig = {
     twitter: {
       site: 'Ecommerce',
       title: 'Istad DealKH | Platform for Cambodian Deals',
-      description: 'Find the best deals, coupons, and promotions from shops in Cambodia. Save big on your shopping with exclusive offers and discounts at DealKH.',
+      description:
+        'Find the best deals, coupons, and promotions from shops in Cambodia. Save big on your shopping with exclusive offers and discounts at DealKH.',
       images: [
-        { url: process.env.NEXT_PUBLIC_LOGO_URL + 'banner.png', alt: 'DealKH Logo Ecommerce Website' },
+        {
+          url: process.env.NEXT_PUBLIC_LOGO_URL + 'banner.png',
+          alt: 'DealKH Logo Ecommerce Website',
+        },
       ],
     },
   },
